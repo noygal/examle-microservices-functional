1ecb14c16ef25b506b3804c7c697ebe1b6035e77
Gal Noy <gal@galnoy.com>
2016-12-06 23:14:40 +0200

    step-3

--- service-posts/server.js
+++ service-posts/server.js
@@ -8,17 +8,17 @@ logger.info('Getting settings')
 core.utils.discovery.create(process.ENV)
 .then(discovery => discovery.get())
 .then(settings => Promise.all([
-  core.utils.server.init(settings.users),
+  core.utils.server.init(settings.posts),
   core.utils.logger.init(settings.logger),
   core.utils.collection.create(settings.mongoDB),
-  settings.users
+  settings.posts
 ]))
 .then(([app, logger, collection, settings]) => {
   logger.info(`Starting server - ${settings.name}`)
   app.use('/', core.middlewares.logs.init(logger))
   app.routeMapper({
     base: settings.base,
-    routes: endpoints.users.init(collection)
+    routes: endpoints.posts.init(collection)
   })
   app.use('*', core.middlewares.errors.init(logger))
   app.listen(settings.port, err => err ? logger.error(err)
